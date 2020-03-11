1bbebf99a45e24b490e08616ec78095c37ea7f56
ethanhsuhsu <ethanhsuhsu@gmail.com>
2020-03-10 17:39:44 -0700

    Temp commit

--- lib/java/com/google/android/material/lists/viewHolders/MaterialViewHolder.java
+++ lib/java/com/google/android/material/lists/viewHolders/MaterialViewHolder.java
@@ -17,4 +17,24 @@ public abstract class MaterialViewHolder extends RecyclerView.ViewHolder {
     protected MaterialViewHolder(int layout, @NonNull ViewGroup parent) {
         this(LayoutInflater.from(parent.getContext()).inflate(layout, parent, false));
     }
+
+  /**
+   * Allows for the MaterialViewHolder to have a divider that is placed at the bottom of the item
+   * @param dividerType Controls the type of divider placed. In some cases, if {@link DividerType#INSET}
+   * is entered, then {@link DividerType#FULL_BLEED} will be used (if the MaterialViewHolder does not
+   * support inset dividers).
+   */
+  public abstract void setDividerType(DividerType dividerType);
+
+  /**
+   * A representation for different dividers used in a {@link MaterialViewHolder}
+   */
+  public enum DividerType {
+    /** Represents the no divider for a list item **/
+    NONE,
+    /** Represents a divider that spans until it reaches a visual or primary action element **/
+    INSET,
+    /** Represents a divider that spans for the entire width of a list item **/
+    FULL_BLEED
+  }
 }
